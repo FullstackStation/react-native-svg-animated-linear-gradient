0db9edad99e86553a9bf1821f3127d5808f2d176
Nhan Nguyen <nhan_nguyen@brangista.com>
2017-06-05 15:45:15 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,93 @@
+## SVG Animated Linear Gradient
+
+This component make Animated Linear Gradient for all SVG components as child props.
+You can use this component as loading component like Facebook or Instagram, used for any group of svg.
+
+## Demo
+![Demo svg animated linear gradient](./images/demo-svg-animated-linear-gradient.gif)
+## Usage
+
+### Examples
+
+#### Instagram style
+```jsx
+<SvgAnimatedLinearGradient height={300}>
+    <Svg.Circle cx="30" cy="30" r="30"/>
+    <Svg.Rect x="75" y="13" rx="4" ry="4" width="100" height="13"/>
+    <Svg.Rect x="75" y="37" rx="4" ry="4" width="50" height="8"/>
+    <Svg.Rect x="0" y="70" rx="5" ry="5" width="400" height="200"/>
+</SvgAnimatedLinearGradient>
+```
+
+#### Facebook style
+
+```jsx
+<SvgAnimatedLinearGradient
+            primaryColor="#e8f7ff"
+            secondaryColor="#4dadf7"
+    height={140}>
+    <Svg.Rect x="0" y="0" rx="5" ry="5" width="70" height="70"/>
+    <Svg.Rect x="80" y="17" rx="4" ry="4" width="300" height="13"/>
+    <Svg.Rect x="80" y="40" rx="3" ry="3" width="250" height="10"/>
+    <Svg.Rect x="0" y="80" rx="3" ry="3" width="350" height="10"/>
+    <Svg.Rect x="0" y="100" rx="3" ry="3" width="200" height="10"/>
+    <Svg.Rect x="0" y="120" rx="3" ry="3" width="360" height="10"/>
+
+</SvgAnimatedLinearGradient>
+```
+#### Code style
+
+```jsx
+<SvgAnimatedLinearGradient
+            primaryColor="#fff0f6"
+            secondaryColor="#f783ac"
+            height={80}>
+    <Svg.Rect x="0" y="0" rx="3" ry="3" width="70" height="10"/>
+    <Svg.Rect x="80" y="0" rx="3" ry="3" width="100" height="10"/>
+    <Svg.Rect x="190" y="0" rx="3" ry="3" width="10" height="10"/>
+    <Svg.Rect x="15" y="20" rx="3" ry="3" width="130" height="10"/>
+    <Svg.Rect x="155" y="20" rx="3" ry="3" width="130" height="10"/>
+    <Svg.Rect x="15" y="40" rx="3" ry="3" width="90" height="10"/>
+    <Svg.Rect x="115" y="40" rx="3" ry="3" width="60" height="10"/>
+    <Svg.Rect x="185" y="40" rx="3" ry="3" width="60" height="10"/>
+    <Svg.Rect x="0" y="60" rx="3" ry="3" width="30" height="10"/>
+</SvgAnimatedLinearGradient>
+```
+### Props
+primaryColor: '#eeeeee',
+    secondaryColor: '#dddddd',
+    duration: 2000,
+    width: 300,
+    height: 200,
+    x1: '0',
+    y1: '0',
+    x2: '100%',
+    y2: '0'
+|Prop   |Type   |Default   |Description
+|---|---|---|---|
+|primaryColor   |String   |'#eeeeee'   |Primary color, also background color   |
+|secondaryColor   |String   |'#dddddd'   |Secondary color   |
+|width   |Number   |300   |Width of SVG   |
+|height   |Number   |200   |Height of SVG   |
+|x1   |String   |'0'   |x of point star gradient, accept Number or Percentage   |
+|y1   |String   |'0'   |y of point star gradient, accept Number or Percentage   |
+|x2   |String   |'100%'   |x of point end gradient, accept Number or Percentage   |
+|y2   |String   |'0'   |y of point end gradient, accept Number or Percentage   |
+
+## TODO
+- Test on Android
+- Write test
+
+## My blog about React/React Native 
+- (Vietnamese only) [Fullstack Station](https://www.businesscard.vn/blog/category/lap-trinh/javascript/react/)
+
+## Thankful
+
+- [Expo](https://expo.io)
+- [React Native Svg](https://github.com/react-native-community/react-native-svg)
+- [Vova Bilonenko](https://github.com/delfrrr) with [his gist](https://gist.github.com/delfrrr/0ef8871c6c4630b406e73fb66c091cf0)
+- [React Content Loader](https://github.com/danilowoz/react-content-loader)
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,143 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import {View, StyleSheet, Animated} from 'react-native';
+
+import Expo, { Svg } from 'expo';
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+const {interpolate} = require('d3-interpolate');
+
+export default class SvgAnimatedLinearGradient extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            offsetValues: [
+                '-2', '-1.5', '-1'
+            ],
+            offsets: [
+                '0', '0', '0'
+            ],
+            frequence: props.duration / 2
+        }
+        this._animate = new Animated.Value(0)
+        this.loopAnimation = this
+            .loopAnimation
+            .bind(this)
+    }   
+    offsetValueBound(x) {
+        if (x > 1) {
+            return '1'
+        }
+        if (x < 0) {
+            return '0'
+        }
+        return x
+    }
+    componentDidMount(props) {
+        this.loopAnimation()
+    }
+
+    loopAnimation() {
+
+        // setup interpolate
+        let interpolator = interpolate(this.state, {
+            offsetValues: ['1', '1.5', '2']
+        });
+
+        // start animation
+        let start = Date.now();
+        this._animation = () => {
+            const now = Date.now();
+            let t = (now - start) / this.props.duration;
+            if (t > 1) {
+                t = 1
+            }
+
+            let newState = interpolator(t);
+            let offsetValues = [];
+            offsetValues[0] = this.offsetValueBound(newState.offsetValues[0]);
+            offsetValues[1] = this.offsetValueBound(newState.offsetValues[1]);
+            offsetValues[2] = this.offsetValueBound(newState.offsetValues[2]);
+            this.setState({offsets: offsetValues});
+            if (t < 1) {
+                requestAnimationFrame(this._animation);
+            }
+        }
+        requestAnimationFrame(this._animation);
+
+        // Setup loop animation
+        Animated.sequence([
+            Animated.timing(this._animate, {
+                toValue: 1,
+                duration: this.state.frequence
+            }),
+            Animated.timing(this._animate, {
+                toValue: 0,
+                duration: this.state.frequence
+            })
+        ]).start((event) => {
+            if (event.finished) {
+                this.loopAnimation()
+            }
+        })
+    }
+    render() {
+
+        return (
+            <AnimatedSvg height={this.props.height} width={this.props.width}>
+                <Svg.Defs>
+                    <Svg.LinearGradient id="grad" x1={this.props.x1} y1={this.props.y1} x2={this.props.x2} y2={this.props.y2}>
+                        <Svg.Stop
+                            offset={this.state.offsets[0]}
+                            stopColor={this.props.primaryColor}
+                            stopOpacity="1"/>
+                        <Svg.Stop
+                            offset={this.state.offsets[1]}
+                            stopColor={this.props.secondaryColor}
+                            stopOpacity="1"/>
+                        <Svg.Stop
+                            offset={this.state.offsets[2]}
+                            stopColor={this.props.primaryColor}
+                            stopOpacity="1"/>
+                    </Svg.LinearGradient>
+                    <Svg.ClipPath id="clip">
+                        <Svg.G>
+                            {this.props.children}
+                        </Svg.G>
+                    </Svg.ClipPath>
+                </Svg.Defs>
+
+                <Svg.Rect
+                    x="0"
+                    y="0"
+                    height={this.props.height}
+                    width={this.props.width}
+                    fill="url(#grad)"
+                    clipPath="url(#clip)"/>
+            </AnimatedSvg>
+        );
+    }
+}
+SvgAnimatedLinearGradient.propTypes = {
+    primaryColor: PropTypes.string,
+    secondaryColor: PropTypes.string,
+    duration: PropTypes.number,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    x1: PropTypes.string,
+    y1: PropTypes.string,
+    x2: PropTypes.string,
+    y2: PropTypes.string,
+}
+SvgAnimatedLinearGradient.defaultProps = {
+    primaryColor: '#eeeeee',
+    secondaryColor: '#dddddd',
+    duration: 2000,
+    width: 300,
+    height: 200,
+    x1: '0',
+    y1: '0',
+    x2: '100%',
+    y2: '0'
+
+}
